44253b2fd315d20db2686e059202b2f9aaba4ccf
kevinsabogal24@gmail.com <kevinsabogal24@gmail.com>
2025-04-20 23:53:06 -0500

    se hacen mejoras de diseño y se agrega navbar

---
+++ script.js
@@ -0,0 +1,41 @@
+const navigation = document.querySelector('.navigation');
+const highlight = document.querySelector('.highlight');
+const items = document.querySelectorAll('.navigation li')
+
+highlight.style.width = items[0].offsetWidth + 'px';
+highlight.style.left = items[0].offsetLeft + 'px';
+
+items.forEach((item, index) => {
+item.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    items.forEach(el => el.classList.remove('active'));
+
+    item.classList.add('active');
+
+    highlight.computedStyleMap.width = item.offsetWidth + 'px';
+    highlight.style.left = item.offsetLeft + 'px';
+});
+
+item.addEventListener('mouseenter', () => {
+    if (!item.classList.contains('active')){
+    highlight.style.width = item.offsetWidth * 0.8 + 'px';
+    highlight.style.left = item.offsetLeft + (item.offsetLeft * 0.1) + 'px';  
+    }
+});
+
+item.addEventListener('mouseleave', () => {
+    const activeItem = document.querySelector('.navigation li.active');
+    highlight.style.width = activeItem.offsetWidth + 'px';
+    highlight.style.left = activeItem.offsetLeft + 'px';  
+});
+
+
+window.addEventListener('resize', () => {
+    const activeItem = document.querySelector('.navigation li.active');
+    highlight.style.width = activeItem.offsetWidth + 'px';
+    highlight.style.left = activeItem.offsetLeft + 'px';
+});
+
+
+});
